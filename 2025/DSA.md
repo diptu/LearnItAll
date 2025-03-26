296ea6c89dcc2a1813d153f94f701830085d5c55
diptu <diptunazmulalam@gmail.com>
2025-03-26 23:24:47 +0600

    2025-03-26

--- 2025/DSA.md
+++ 2025/DSA.md
@@ -120,6 +120,10 @@
 - [205. Isomorphic Strings](https://leetcode.com/problems/isomorphic-strings/description/?envType=study-plan-v2&envId=top-interview-150)
 - [242. Valid Anagram](https://leetcode.com/problems/valid-anagram/description/?envType=study-plan-v2&envId=top-interview-150)
 
+**2025-03-26**
+- [290. Word Pattern](https://leetcode.com/problems/word-pattern/description/?envType=study-plan-v2&envId=top-interview-150)
+
+
 ### Linked List
 **2025-03-22**
 - [206. Reverse Linked List](https://leetcode.com/problems/reverse-linked-list/description/)
--- 2025/DSA/HashTable/205.cpp
+++ 2025/DSA/HashTable/205.cpp
@@ -1,68 +1,88 @@
+
 /*
-205. Isomorphic Strings
-Hash Maps – O(n) Time and O(MAX_CHAR) Space
-MAX_CHAR is maximum number of different characters in the two strings. If we talk about lower case alphabet, then its value is 26
-
-The idea is based on the fact that all occurrences of two characters should be at same index. We mainly store the first index of every character and for remaining occurrences, we check if they appear at same first index too.
-
-We mainly use two maps m1 and m2 to store characters as keys and their first indexes as values.
-
-If the lengths of s1 and s2 are not same, return false.
-Do the following for every character in s1 and s2.
-If this character is seen first time in s1, then store is index in map m1.
-If this character is seen first time in s2, then store is index in map m2.
-If indexes in map for both the characters do not match, return false.
+290. Word Pattern
 */
-
 #include <bits/stdc++.h>
 using namespace std;
-#define SIZE 256 // SIZE is maximum number of different characters in the two strings
-bool isIsomorphic(string s, string t)
+class Solution
 {
-
-    if (s.length() != t.length())
-        return false;
-
-    unordered_map<char, int> m1, m2;
-
-    for (int i = 0; i < s.length(); ++i)
+public:
+    vector<string> split_sentence(string sen)
     {
 
-        // If character not seen before,
-        if (m1.find(s[i]) == m1.end())
-        {
-            // store its  first occurrence index
-            m1[s[i]] = i;
+        // Create a stringstream object
+        stringstream ss(sen);
 
-            // m1 hash table
-            // 'a' : 0
-            // b : 2
+        // Variable to hold each word
+        string word;
+
+        // Vector to store the words
+        vector<string> words;
+
+        // Extract words from the sentence
+        while (ss >> word)
+        {
+
+            // Add the word to the vector
+            words.push_back(word);
         }
 
-        // If character not seen before,
-        if (m2.find(t[i]) == m2.end())
-        {
-            // store its  first occurrence index
-            m2[t[i]] = i;
-
-            // m2 hash table
-            // 'x' : 0
-            // y : 2
-        }
-
-        // Check if the first occurrence indices match
-        if (m1[s[i]] != m2[t[i]])
-        {
-            return false;
-        }
+        return words;
     }
-    return true;
-}
+
+    bool wordPattern(string pattern, string s)
+    {
+
+        // Call the function to split the sentence
+        vector<string> words = split_sentence(s);
+
+        // if number of char in pattern dosen't match
+        // with number of words in words
+        if (pattern.size() != words.size())
+            return false;
+
+        unordered_map<char, string> m1;
+        unordered_map<string, char> m2;
+
+        // iterate over pattern
+        for (int i = 0; i < pattern.size(); i++)
+        {
+            char ch = pattern[i];
+            string word = words[i];
+
+            // check if ch already exist in m1
+            if (m1.count(ch))
+            {
+
+                // value doesn't match with current word
+                if (m1[ch] != word)
+                    return false;
+            }
+            // if ch doesn't exist in m1,
+            else
+            {
+                // check if word already exist in m2
+                if (m2.count(word))
+                {
+
+                    // value doesn't match with current ch
+                    if (m2[word] != ch)
+                        return false;
+                }
+                // first ocurance update hash table for both m1 & m2
+                m1[ch] = word;
+                m2[word] = ch;
+            }
+        }
+        return true;
+    }
+};
 int main()
 {
-    string s = "aab", t = "xxy";
+    Solution sol;
+    string pattern = "abba", s = "dog cat cat dog";
 
-    cout << isIsomorphic(s, t);
+    cout << sol.wordPattern(pattern, s);
 
     return 0;
 }
